9c49a002f82dc4918011fd5cbe119e87f6da18f8
Surbhi Zambad <surbhiszambad@gmail.com>
2020-06-17 17:50:13 -0500

    Created a function for tracking the month and year

--- Functions.py
+++ Functions.py
@@ -13,6 +13,7 @@ for elements in a_list:
     sum_manual += elements
 
 print(sum_manual)
+# using sum() function
 print(sum(a_list))
 
 # UPDATE THE VALUES IN CONTENT RATING DICTIONARY
@@ -36,3 +37,54 @@ def square(a_num):
 
 print(square(10))
 print(square(16))
+
+# creating a function with arguments
+
+
+def personal_details(name, branch="CSE"):
+    return '{} is my name, {} is my branch'.format(name, branch)
+
+
+print(personal_details("Surbhi"))
+
+# creating a functions with arguments which can have default multiple values
+
+
+def student_details(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+
+student_details('CSE', 'UTA', 'Arlington',
+                name="Surbhi", last="Zambad", age=26)
+
+# creating a functions with arguments which can have multiple values
+
+branch_uni_city = ['CSE', 'UTA', 'Arlington']
+info = {'name': "Surbhi", 'last': "Zambad", 'age': 26}
+
+student_details(*branch_uni_city, **info)
+
+# creating a calender functionality which determines whether the year is leap or not and defines the days in month
+
+month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
+def is_leap(year):
+    '''returns true if the year is leap year and false if year is not leap year'''
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+
+def days_in_month(year, month):
+    '''returns number of days in that month and in that year'''
+
+    if not 1 <= month <= 12:
+        print("Invalid month")
+
+    if month == 2 and is_leap(year):
+        return 29
+
+    return month_days[month]
+
+
+print(days_in_month(2017, 12))
