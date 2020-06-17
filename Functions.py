d9470b280fdc75320b2c7189dbe7c616158e3776
Surbhi Zambad <surbhiszambad@gmail.com>
2020-06-17 15:06:08 -0500

    Implemented built-in functions

---
+++ Functions.py
@@ -0,0 +1,21 @@
+# SUM OF NUMBERS IN THE LIST
+
+a_list = [5, 10, 15, 20, 25]
+
+sum_manual = 0
+for elements in a_list:
+    sum_manual += elements
+
+print(sum_manual)
+print(sum(a_list))
+
+# UPDATE THE VALUES IN CONTENT RATING DICTIONARY
+
+ratings = ['4+', '4+', '4+', '9+', '12+', '12+', '17+', '17+']
+content_ratings = {}
+for rating in ratings:
+    if rating in content_ratings:
+        content_ratings[rating] += 1
+    else:
+        content_ratings[rating] = 1
+print(content_ratings)
