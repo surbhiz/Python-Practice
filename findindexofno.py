88ec789b82eeda5d02890cba7aea02b01fd7387d
Surbhi Zambad <surbhiszambad@gmail.com>
2020-06-24 14:07:07 -0500

    find the indices of the number in the given list

---
+++ findindexofno.py
@@ -0,0 +1,10 @@
+list_of_numbers = [[1, 2, 3, 1], 1, [1, 2]]
+indices = []
+
+for num in range(len(list_of_numbers)):
+
+    if list_of_numbers[num] == 1:
+        indices.append(num)
+
+
+print(indices)
