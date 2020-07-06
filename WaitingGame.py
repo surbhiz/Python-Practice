722e2f7441bc1d07212253ae4884d8a8b6daf830
Surbhi Zambad <surbhiszambad@gmail.com>
2020-07-06 14:17:24 -0500

    Changed the time function

--- WaitingGame.py
+++ WaitingGame.py
@@ -10,17 +10,17 @@ def time_convert(sec):
     if time_lapsed == 4:
         print("you are on time")
     elif time_lapsed > 4:
-        print("You are too fast")
+        print("You are too slow")
     else:
-        print("You are slow")
+        print("You are too fast")
 
 
 input("Press Enter to start")
-start_time = time.time()
-print("You started at:", start_time)
+start_time = time.gmtime()
+print("You started at:", start_time[5])
 input("Press Enter to stop")
-end_time = time.time()
-print("You ended at:", end_time)
+end_time = time.gmtime()
+print("You ended at:", end_time[5])
 
-time_lapsed = end_time - start_time
+time_lapsed = end_time[5] - start_time[5]
 time_convert(time_lapsed)
